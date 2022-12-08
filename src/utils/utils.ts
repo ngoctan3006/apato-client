b368f76cbe651849dc2312efc055dde5d5d81621
Hoang Tien Dat <hoangtiendat.thidaihoc@gmail.com>
2022-12-08 15:36:51 +0700

    Add commas to price number

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,9 @@
 export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+function numberWithCommas(x: number) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+export {
+  numberWithCommas
+}
