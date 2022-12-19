e57eb6215d3fe75b24c909cb6f5a28623e59ff66
robert.hoang <robert.hoang@avepoint.com>
2022-12-19 17:09:53 +0700

    Update Sprint 4

--- src/api/client.ts
+++ src/api/client.ts
@@ -7,7 +7,6 @@ const apiClient = axios.create({
     'Access-Control-Allow-Origin' : '*',
     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
   },
-  withCredentials: true,
   responseType: "json",
   timeout: 20000,
 })
