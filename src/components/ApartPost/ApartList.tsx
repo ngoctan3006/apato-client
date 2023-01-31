0ad05a4a820e7cb12cbc8b174e85d3297498dba0
Tan <ngoctana1k2014@gmail.com>
2023-01-31 10:21:16 +0700

    no data display message

--- src/components/ApartPost/ApartList.tsx
+++ src/components/ApartPost/ApartList.tsx
@@ -26,9 +26,20 @@ interface ApartListProps
 const ApartList: React.FC<ApartListProps> = (props) => {
   return (
     <Grid container p="40px 0 0 40px" spacing={2} alignItems="stretch">
-      {props.data.map((item) => (
-        <Apart key={item.id} item={item} />
-      ))}
+      {props.data.length ? (
+        props.data.map((item) => <Apart key={item.id} item={item} />)
+      ) : (
+        <Typography
+          variant="h6"
+          color="textSecondary"
+          sx={{
+            width: '100%',
+            textAlign: 'center',
+          }}
+        >
+          Không có dữ liệu
+        </Typography>
+      )}
     </Grid>
   );
 };
